a3399ad045f0aeec8b89b2f65173b50b740b961c
Nathalia Lafosse <lafossenathalia@gmail.com>
2021-11-17 14:16:41 -0500

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import contacts from "./contacts.json";
 import './App.css';
+
+let fiveCelebs = contacts.slice(0, 5);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+
+        {fiveCelebs.map(celeb => {
+          return (
+            <tr>
+              <td><img src={celeb.pictureUrl} height="100" /></td>
+              <td>{celeb.name}</td>
+              <td>{celeb.popularity}</td>
+            </tr>
+          )
+        })}
+
+        
+
+      </table>
     </div>
   );
 }
+
 
 export default App;
