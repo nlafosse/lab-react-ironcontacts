c312b646df3e97a6b4a1747c2fd7805fd6cb08d0
Nathalia Lafosse <lafossenathalia@gmail.com>
2021-11-17 19:00:30 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,77 @@ import React, {useState} from 'react';
 import contacts from "./contacts.json";
 import './App.css';
 
-let fiveCelebs = contacts.slice(0, 5);
+let newContacts = [...contacts]
+let fiveContacts = contacts.slice(0, 4);
 
 function App() {
+  const [contactArr, setContactArr] = useState(fiveContacts)
+
+  function sortButtons(button){
+    let sortArr = [...contactArr]
+    if (button === 'name'){
+      sortArr.sort(function(a, b) {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      })
+    }
+    if (button === 'pop') {
+      sortArr.sort((a, b) => a.popularity - b.popularity);
+    }
+    setContactArr(sortArr)
+  }
+
+  // function sortNames(){
+  //   let sortArr = [...contactArr]
+  //   sortArr.sort(function(a, b) {
+  //     if (a.name < b.name) {
+  //       return -1;
+  //     }
+  //     if (a.name > b.name) {
+  //       return 1;
+  //     }
+  //     // names must be equal
+  //     return 0;
+  //   })
+  //   setContactArr(sortArr)
+  // }
+
+  // function sortPop(){
+  //   let sortArr = [...contactArr]
+  //   sortArr.sort((a, b) => a.popularity - b.popularity);
+  //   setContactArr(sortArr)
+  // }
+
+  function deleteContact(person){
+    let removePersonArr = [...contactArr]
+    setContactArr(removePersonArr.filter(contact => contact.id != person.id))
+    
+  }
+
   return (
     <div className="App">
+      <h1>Iron Concacts</h1>
+
+      <div className="topButtons">
+        <button type="button" onClick={() =>{
+          let i = Math.floor(Math.random()*newContacts.length)
+          setContactArr(contactArr.concat(newContacts[i]))
+          newContacts.splice(i, 1)
+        }}>Add Random Contact</button>
+      
+        <button onClick={() => sortButtons('name')}>Sort by name</button>
+      
+        <button onClick={() => sortButtons('pop')}>Sort by popularity</button>
+      </div>
+
+      
+
       <table>
         <tr>
           <th>Picture</th>
@@ -14,12 +80,13 @@ function App() {
           <th>Popularity</th>
         </tr>
 
-        {fiveCelebs.map(celeb => {
+        {contactArr.map(person => {
           return (
             <tr>
-              <td><img src={celeb.pictureUrl} height="100" /></td>
-              <td>{celeb.name}</td>
-              <td>{celeb.popularity}</td>
+              <td><img src={person.pictureUrl} height="100" /></td>
+              <td>{person.name}</td>
+              <td>{person.popularity}</td>
+              <td><button onClick={() => deleteContact(person)}>Delete</button></td>
             </tr>
           )
         })}
